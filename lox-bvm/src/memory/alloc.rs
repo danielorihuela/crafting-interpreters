896fa5d9a57639db3cffcec579967fcb4ada7b3d
dani <danielorihuelarodriguez@gmail.com>
2026-09-12 16:47:37 +0200

    refactor: reallocate

--- lox-bvm/src/memory/alloc.rs
+++ lox-bvm/src/memory/alloc.rs
@@ -33,45 +33,35 @@ pub fn allocate<T>(size: usize) -> *mut T {
 }
 
 pub fn reallocate<T>(ptr: *mut T, old_capacity: usize, new_capacity: usize) -> *mut T {
-    let old_size = old_capacity.saturating_mul(std::mem::size_of::<T>());
-    let new_size = new_capacity.saturating_mul(std::mem::size_of::<T>());
-
-    adjust_bytes_allocated(old_size, new_size);
-
-    if new_capacity > old_capacity && vm_is_ready() {
-        if DEBUG_STRESS_GC {
-            garbage_collect();
-        }
-
-        if unsafe { (*VM_INSTANCE).bytes_allocated > (*VM_INSTANCE).next_gc } {
-            garbage_collect();
-        }
-    }
-
-    let new_layout = Layout::array::<T>(new_capacity).unwrap();
-    if old_capacity == 0 {
-        return unsafe { alloc(new_layout) } as *mut T;
-    }
-
-    let old_layout = Layout::array::<T>(old_capacity).unwrap();
-    if new_capacity == 0 {
-        unsafe { dealloc(ptr as *mut u8, old_layout) }
-        return std::ptr::null_mut();
-    };
-
-    let ret = unsafe { realloc(ptr as *mut u8, old_layout, new_layout.size()) };
-    if ret.is_null() {
-        panic!("Memory reallocation failed");
-    }
-
-    ret as *mut T
+    reallocate_inner(ptr, old_capacity, new_capacity, true)
 }
 
 pub fn reallocate_no_gc<T>(ptr: *mut T, old_capacity: usize, new_capacity: usize) -> *mut T {
+    reallocate_inner(ptr, old_capacity, new_capacity, false)
+}
+
+fn reallocate_inner<T>(
+    ptr: *mut T,
+    old_capacity: usize,
+    new_capacity: usize,
+    gc_enabled: bool,
+) -> *mut T {
     let old_size = old_capacity.saturating_mul(std::mem::size_of::<T>());
     let new_size = new_capacity.saturating_mul(std::mem::size_of::<T>());
 
     adjust_bytes_allocated(old_size, new_size);
+
+    if gc_enabled {
+        if new_capacity > old_capacity && vm_is_ready() {
+            if DEBUG_STRESS_GC {
+                garbage_collect();
+            }
+
+            if unsafe { (*VM_INSTANCE).bytes_allocated > (*VM_INSTANCE).next_gc } {
+                garbage_collect();
+            }
+        }
+    }
 
     let new_layout = Layout::array::<T>(new_capacity).unwrap();
     if old_capacity == 0 {
